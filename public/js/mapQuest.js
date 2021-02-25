5bf37b684c2f7c9f2f05c69a8145fc761f9c65d3
Kyu Eun <59637285+MOCHIDDEOK@users.noreply.github.com>
2021-02-25 10:40:34 -0800

    yay

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '60XGglIRrRSTU9m8v4QyVjJIICrOAnYA';
+	var map = L.mapquest.map('map', {
+		center: [32.878727518238684, -117.23583235867751],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.878727518238684, -117.23583235867751]).addTo(map);
 }
